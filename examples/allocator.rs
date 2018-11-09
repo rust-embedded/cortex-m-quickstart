f7a943f480dbf2268363783891c47613db6b74b7
Jorge Aparicio <jorge@japaric.io>
2018-11-10 00:01:25 +0100

    fix the allocator example
    importing alloc is a bit weird

--- examples/allocator.rs
+++ examples/allocator.rs
@@ -14,11 +14,12 @@
 #![no_main]
 #![no_std]
 
+extern crate alloc;
 extern crate panic_halt;
 
+use self::alloc::vec;
 use core::alloc::Layout;
 
-use alloc::vec;
 use alloc_cortex_m::CortexMHeap;
 use cortex_m::asm;
 use cortex_m_rt::entry;
@@ -39,6 +40,10 @@ fn main() -> ! {
     let xs = vec![0, 1, 2];
 
     hprintln!("{:?}", xs).unwrap();
+
+    // exit QEMU
+    // NOTE do not run this on hardware; it can corrupt OpenOCD state
+    debug::exit(debug::EXIT_SUCCESS);
 
     loop {}
 }
