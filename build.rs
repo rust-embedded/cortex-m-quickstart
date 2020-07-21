541c7df215bc3982bc6a77a811c8748a2833a245
Daniel Egger <daniel@eggers-club.de>
2020-07-22 00:27:48 +0200

    Update build.rs
    Co-authored-by: Jonas Schievink <jonasschievink@gmail.com>

--- build.rs
+++ build.rs
@@ -27,4 +27,5 @@ fn main() {
     // any file in the project changes. By specifying `memory.x`
     // here, we ensure the build script is only re-run when
     // `memory.x` is changed.
-    println!("cargo:rerun-if-changed=memory.x"); }
+    println!("cargo:rerun-if-changed=memory.x");
+}
