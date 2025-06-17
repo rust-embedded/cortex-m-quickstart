6aa25500e63e8bd6c1b20d25147d0bf1e3ab9aa1
Adam Greig <adam@ael.co.uk>
2025-06-17 19:11:13 +0100

    Remove unnecessary nop from main loops

--- examples/test_on_host.rs
+++ examples/test_on_host.rs
@@ -28,14 +28,11 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
 
 #[cfg(not(test))]
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-
     loop {
         // your code goes here
     }
